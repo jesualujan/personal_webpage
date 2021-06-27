8e58b2a4de54e3944be48a0d78f53d0ac545f38b
Jesua Luján <code.masterqro@gmail.com>
2021-06-27 18:33:59 -0500

    trabajando animaciones de inicio

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+TweenMax.to(".overlay" , 2 , {
+    delay: '2',
+    top: '-100%',
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.to("overlay-text" ,2 , {
+    delay: '1',
+    opacity: '1',
+    y: '-60',
+    ease: Expo.easeInOut
+})
